712ef91cdda8dc2c79ee6f9b43a464788ceee923
Me <jeremiah.brock10@gmail.com>
2023-02-07 11:10:19 -0500

    Added Profile

--- Anti-Social Business Club/Profile.swift
+++ Anti-Social Business Club/Profile.swift
@@ -9,13 +9,54 @@ import SwiftUI
 
 struct Profile: View {
     var body: some View {
+        
         ZStack{
-        Color.teal
-            .ignoresSafeArea()
-            .foregroundColor(.white)
             
+            Color.teal
+                .ignoresSafeArea()
+            VStack{
+              //  Image(systemName: "plus.rectangle")
+                    
+                    
+                Rectangle()
+                
+                    .foregroundColor(.white)
+                    .frame(width: 400, height: 250)
+                Spacer()
+                
+                Text("Create A Bio")
+                    .bold()
+                Spacer()
+                Text("Add Photos of Your Business")
+                Spacer()
+                
+                Grid {
+                    GridRow {
+                        GridRow {
+                            Rectangle()
+                                .frame(width: 102, height: 109)
+                            Rectangle()
+                                .frame(width: 102, height: 109)
+                            Rectangle()
+                                .frame(width: 102, height: 109)
+                            Spacer()
+                        }
+                    }
+                    GridRow {
+                        Rectangle()
+                            .frame(width: 102, height: 109)
+                        Rectangle()
+                            .frame(width: 102, height: 109)
+                        Rectangle()
+                            .frame(width: 102, height: 109)
+                    }
+                    Spacer()
+                }
+                
+            }
         }
     }
+    
 }
 
 struct Profile_Previews: PreviewProvider {
